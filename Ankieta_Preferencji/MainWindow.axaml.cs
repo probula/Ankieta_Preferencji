42660ab62447a397d90436f142d8b2344b523fd7
igor <igor.kowalczyk07@poczta.fm>
2025-03-04 19:05:51 +0100

    handling data from question fields

--- Ankieta_Preferencji/MainWindow.axaml.cs
+++ Ankieta_Preferencji/MainWindow.axaml.cs
@@ -16,4 +16,15 @@ public partial class MainWindow : Window
         var comboBoxValue = (comboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "No selection";
         ShowTextBlock2.Text = comboBoxValue;
     }
+
+    private void SubmitTwo(object? sender, RoutedEventArgs e)
+    {
+        var checkBoxValue1 = Option1.IsChecked == true ? "Checked" : "Unchecked";
+        var checkBoxValue2 = Option2.IsChecked == true ? "Checked" : "Unchecked";
+        var checkBoxValue3 = Option3.IsChecked == true ? "Checked" : "Unchecked";
+        
+        ShowTextBlock3.Text = checkBoxValue1;
+        ShowTextBlock4.Text = checkBoxValue2;
+        ShowTextBlock5.Text = checkBoxValue3;
+    }
 }
