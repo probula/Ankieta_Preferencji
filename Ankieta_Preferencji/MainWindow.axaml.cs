b246b56eb4f94adbf8f455d1f31fcbdaf1c22a53
igor <igor.kowalczyk07@poczta.fm>
2025-03-04 18:45:20 +0100

    data handling from name and interests fields

--- Ankieta_Preferencji/MainWindow.axaml.cs
+++ Ankieta_Preferencji/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Ankieta_Preferencji;
 
@@ -7,5 +8,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void SubmitOne(object? sender, RoutedEventArgs e)
+    {
+        ShowTextBlock1.Text = inputName.Text;
+        var comboBoxValue = (comboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "No selection";
+        ShowTextBlock2.Text = comboBoxValue;
     }
 }
